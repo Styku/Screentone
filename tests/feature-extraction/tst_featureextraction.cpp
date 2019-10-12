319ddd3c5e7143dc16c54aafb5797d82e37014fd
Bartlomiej Styczen <brtstyczen@gmail.com>
2019-10-12 15:45:22 +0200

    feature extraction test cases

--- tests/feature-extraction/tst_featureextraction.cpp
+++ tests/feature-extraction/tst_featureextraction.cpp
@@ -5,6 +5,9 @@
 
 #include "textfeatures.h"
 
+using cv::Rect;
+using std::vector;
+
 class FeatureExtraction : public QObject
 {
     Q_OBJECT
@@ -13,9 +16,19 @@ public:
     FeatureExtraction();
     ~FeatureExtraction();
 
-private slots:
-    void test_case1();
+private:
+    TextFeatures tf;
+    NormalizedFeatures nf;
+    static constexpr double screen_area = 640*480;
 
+private slots:
+    void initTestCase();
+    void test_coverage();
+    void test_max_area();
+    void test_median_area();
+    void test_median_width();
+    void test_placement_histogram();
+    void cleanupTestCase();
 };
 
 FeatureExtraction::FeatureExtraction()
@@ -28,7 +41,41 @@ FeatureExtraction::~FeatureExtraction()
 
 }
 
-void FeatureExtraction::test_case1()
+void FeatureExtraction::initTestCase()
+{
+    vector<Rect> v{Rect(0, 0, 200, 200), Rect(250, 250, 50, 20), Rect(50, 50, 50, 50), Rect(620, 0, 20, 20)};
+    tf = TextFeatures(v, 640, 480);
+    nf = tf.normalize();
+}
+
+void FeatureExtraction::test_coverage()
+{
+    QCOMPARE(nf["coverage"], (200*200 + 50*20 + 50*50 + 20*20)/screen_area);
+}
+
+void FeatureExtraction::test_max_area()
+{
+    QCOMPARE(nf["max_area"], (200*200)/screen_area);
+}
+
+void FeatureExtraction::test_median_area()
+{
+    QCOMPARE(nf["median_area"], ((50*50 + 50*20)/2)/screen_area);
+}
+
+void FeatureExtraction::test_median_width()
+{
+    QCOMPARE(nf["median_width"], 50/640.0);
+}
+
+void FeatureExtraction::test_placement_histogram()
+{
+    QCOMPARE(nf["placement_0"], 2);
+    QCOMPARE(nf["placement_3"], 1);
+    QCOMPARE(nf["placement_9"], 1);
+}
+
+void FeatureExtraction::cleanupTestCase()
 {
 
 }
