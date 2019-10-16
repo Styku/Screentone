a862c59e98819019526f45f01a2738c045e1df09
Bartlomiej Styczen <brtstyczen@gmail.com>
2019-10-16 17:05:49 +0200

    softmax function

--- src/libtxtdet/helpers.h
+++ src/libtxtdet/helpers.h
@@ -3,7 +3,11 @@
 
 #endif // HELPERS_H
 
+#include <cmath>
 #include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <functional>
 #include <vector>
 
 template<template<typename, typename> class Container, typename T>
@@ -15,4 +19,13 @@ double median(Container<T, std::allocator<T>> v)
     if(v.size()%2 == 0)
         m = (m + *(std::max_element(v.begin(), v.begin() + nth)))/2;
     return m;
+}
+
+template<typename It>
+void softmax(It begin, It end)
+{
+    using T = typename std::iterator_traits<It>::value_type;
+    T sum{0};
+    std::transform(begin, end, begin, [&](T val){ auto e = std::exp(val); sum += e; return e; });
+    std::transform(begin, end, begin, std::bind2nd(std::divides<T>(), sum));
 }
