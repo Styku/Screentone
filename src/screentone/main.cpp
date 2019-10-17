4af253ce612b2417af48fa34876f2fea7f7c2690
Bartlomiej Styczen <brtstyczen@gmail.com>
2019-10-17 20:44:58 +0200

    adapt to ActivityRecognition changes

--- src/screentone/main.cpp
+++ src/screentone/main.cpp
@@ -15,12 +15,9 @@ using namespace cv;
 
 int main()
 {
-    ScreenParams scr{"DP-1-1"};
-    ActivityRecognition activity;
-    activity.load("model.yaml");
-    //cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::load("model.yaml");
-
-
+    ScreenParams scr{"eDP-1-1"};
+    ActivityRecognition activity("model.yaml");
+    activity.setCategories(std::vector<std::string>{"reading", "social-media", "coding"});
 
     Screencap screencap(0, 0, 1920, 1080);
     TextDetector txtd = TextDetector{}.setLetterSpacing(17, 3).setParagraphSpacing(27, 31).setGrouping(TextGrouping::Paragraphs).setThreshold(220);
@@ -41,20 +38,20 @@ int main()
         NormalizedFeatures X = textfeat.normalize();
 
         std::string ac = activity.predict(X);
+
         boxes = textfeat.getTextBoxes();
         for(auto& box : boxes)
         {
             cv::rectangle(frame, box, Scalar(0, 255, 0));
         }
 
-        // Just for testing
-        if(X["coverage"] > 0.3 && mode == 0)
+        if((ac == "coding" || ac == "reading") && mode == 0)
         {
             cout << "Lowering gamma, coverage: " << X["coverage"] << ", max. area: " << X["max_area"] << endl;
             currentTemp = std::async(std::launch::async, &ScreenParams::setTemperature, &scr, 2800, 1000);
             mode = 1;
         }
-        else if(X["coverage"] <= 0.3 && mode == 1)
+        else if(ac == "social-media" && mode == 1)
         {
             cout << "Increasing gamma, coverage: " << X["coverage"] << ", max. area: " << X["max_area"] << endl;
             currentTemp = std::async(std::launch::async, &ScreenParams::setTemperature, &scr, 6600, 1000);
